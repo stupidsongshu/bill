6e75e1e1b3a413a7a8faf9d7791fbe308a4353a5
yusong zhou <yusong.zhou@maimob.cn>
2022-07-01 23:25:31 +0800

    用户新增

--- admin-pro/src/pages/list/user-list/index.tsx
+++ admin-pro/src/pages/list/user-list/index.tsx
@@ -1,10 +1,13 @@
-// import React from 'react'
+import { useState, useRef } from 'react'
 import type { FC } from 'react'
+import { Button, message } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
 import { PageContainer } from '@ant-design/pro-layout'
 import ProTable from '@ant-design/pro-table'
 import type { ProColumns, ActionType } from '@ant-design/pro-table'
+import { ModalForm, ProFormSelect, ProFormText } from '@ant-design/pro-form'
 import type { TableListItem, TableListParams } from './data'
-import { getUserPageList} from './service'
+import { getUserPageList, userSave } from './service'
 
 const columns: ProColumns<TableListItem>[] = [
   {
@@ -81,9 +84,27 @@ const columns: ProColumns<TableListItem>[] = [
 ]
 
 const UserList: FC = () => {
+  const actionRef= useRef<ActionType>()
+  const [createModalVisible, handleModalVisible] = useState<boolean>(false)
+
+  async function handleAdd(fields: TableListItem): Promise<boolean> {
+    const hide = message.loading('正在添加')
+    try {
+      await userSave(fields)
+      hide()
+      message.success('添加成功')
+      return true
+    } catch (error) {
+      hide()
+      message.error('添加失败请重试！')
+      return false
+    }
+  }
+
   return <PageContainer>
     <ProTable<TableListItem, TableListParams>
       headerTitle='查询用户列表'
+      actionRef={actionRef}
       cardBordered
       rowKey='id'
       rowSelection={{}}
@@ -91,8 +112,68 @@ const UserList: FC = () => {
       editable={{
         type: 'single'
       }}
-      request={getUserPageList}
-      ></ProTable>
+      toolBarRender={() => [
+        <Button
+          type="primary"
+          onClick={() => {
+            handleModalVisible(true)
+          }}
+        >
+          <PlusOutlined /> 新建
+        </Button>
+      ]}
+      request={getUserPageList}>
+    </ProTable>
+
+    <ModalForm
+      title="新建"
+      width={500}
+      layout="horizontal"
+      labelCol={{span: 4}}
+      wrapperCol={{span: 20}}
+      visible={createModalVisible}
+      onVisibleChange={handleModalVisible}
+      onFinish={async (values) => {
+        const success = await handleAdd(values as TableListItem)
+        if (success) {
+          handleModalVisible(false)
+          if (actionRef.current) {
+            actionRef.current.reload()
+          }
+        }
+      }}
+      >
+      <ProFormText
+        label="用户名"
+        name="name"
+        width="md"
+        rules={[
+          { required: true, message: '请输入用户名' }
+        ]}
+      >
+      </ProFormText>
+      <ProFormText.Password
+        label="密码"
+        name="password"
+        width='md'
+        rules={[
+          { required: true, message: '请输入密码' }
+        ]}>
+      </ProFormText.Password>
+      <ProFormText label="手机号" name="phone" width="md"></ProFormText>
+      <ProFormText label="邮箱" name="email" width="md"></ProFormText>
+      <ProFormSelect
+        label="状态"
+        name="status"
+        width='md'
+        valueEnum={{
+          0: '无效',
+          1: '正常'
+        }}
+        initialValue="1"
+        placeholder="请选择用户状态"
+      ></ProFormSelect>
+    </ModalForm>
   </PageContainer>
 }
 
--- admin-pro/src/pages/list/user-list/service.ts
+++ admin-pro/src/pages/list/user-list/service.ts
@@ -1,10 +1,18 @@
 import { request } from 'umi'
-import { TableListParams, TableListData } from './data'
+import { TableListParams, TableListData, TableListItem } from './data'
 
 /** 获取用户列表 POST /api/user/pageList */
 export async function getUserPageList(data: TableListParams, options?: { [key: string]: any }) {
   return request<TableListData>('/api/user/pageList', {
     method: 'POST',
+    data,
+    ...(options || {})
+  })
+}
+
+export async function userSave(data: TableListItem, options?: { [key: string]: any }) {
+  return request<boolean>('/api/user/save', {
+    method: 'post',
     data,
     ...(options || {})
   })
